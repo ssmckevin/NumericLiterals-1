714a14a091297ae3ebbc61af799c69883c65df04
BVolt <Brendenj7@hotmail.com>
2023-11-13 23:46:03 -0800

    fixing comments

--- FloatingPointDFA.py
+++ FloatingPointDFA.py
@@ -1,13 +1,13 @@
 class FloatingPointDFA:
-    states = {"q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12" } # Set of States of decimal integer DFA
-    acceptstates = {"q5", "q6", "q8"} # Set of accept States of decimal integer DFA
-    start_state = "q1" # initial state
+    states = {"q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12" } 
+    acceptstates = {"q5", "q6", "q8"} 
+    start_state = "q1" 
     alphabet = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_", ".", "E", "e", "+", "-"}
     digit = alphabet - {"_",  ".", "E", "e", "+", "-"}
     transition = {}
 
     def __init__(self):
-        self.transition = { # transition function for all states
+        self.transition = {
             "q1": lambda c: "q2" if c in self.digit else("q4" if c=="." else "q12"), 
             "q2": lambda c: "q2" if c in self.digit else("q3" if c=="_" else ("q5" if c=="." else ("q7" if c=="e" or c=="E"else "q12"))),
             "q3": lambda c: "q2" if c in self.digit else "q12",
@@ -23,12 +23,12 @@ class FloatingPointDFA:
         }
     
     def accepts(self, str):
-        state = self.start_state # set start state
-        for char in str: # Step through characters in input
-            state = self.transition[state](char) # apply lambda based on state
+        state = self.start_state
+        for char in str: 
+            state = self.transition[state](char) 
             
         if state in self.acceptstates: 
-            return True # accept if final state is in accept states
+            return True 
         else:
             return False
         
